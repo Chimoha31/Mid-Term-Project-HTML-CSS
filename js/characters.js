6848b2d78e4a165f1f148fb601a69dbf41429e4d
Chimoha31 <mokochii1108@gmail.com>
2021-12-15 01:45:58 -0800

    add images

--- js/characters.js
+++ js/characters.js
@@ -2,23 +2,149 @@
 {
   const open = document.querySelector(".open");
   const open02 = document.querySelector(".open02");
+  const open03 = document.querySelector(".open03");
+  const open04 = document.querySelector(".open04");
+  const open05 = document.querySelector(".open05");
+  const open06 = document.querySelector(".open06");
+  const open07 = document.querySelector(".open07");
+  const open08 = document.querySelector(".open08");
+  const open09 = document.querySelector(".open09");
+  const open10 = document.querySelector(".open10");
+  const open11 = document.querySelector(".open11");
+  const open12 = document.querySelector(".open12");
+  const open13 = document.querySelector(".open13");
+  const open14 = document.querySelector(".open14");
+  const open15 = document.querySelector(".open15");
+  const open16 = document.querySelector(".open16");
+  const open17 = document.querySelector(".open17");
+
+
   const mask = document.getElementById("mask");
   const mask02 = document.getElementById("mask02");
+  const mask03 = document.getElementById("mask03");
+  const mask04 = document.getElementById("mask04");
+  const mask05 = document.getElementById("mask05");
+  const mask06 = document.getElementById("mask06");
+  const mask07 = document.getElementById("mask07");
+  const mask08 = document.getElementById("mask08");
+  const mask09 = document.getElementById("mask09");
+  const mask10 = document.getElementById("mask10");
+  const mask11 = document.getElementById("mask11");
+  const mask12 = document.getElementById("mask12");
+  const mask13 = document.getElementById("mask13");
+  const mask14 = document.getElementById("mask14");
+  const mask15 = document.getElementById("mask15");
+  const mask16 = document.getElementById("mask16");
+  const mask17 = document.getElementById("mask17");
+
+
   const modal = document.getElementById("modal");
   const modal02 = document.getElementById("modal02");
+  const modal03 = document.getElementById("modal03");
+  const modal04 = document.getElementById("modal04");
+  const modal05 = document.getElementById("modal05");
+  const modal06 = document.getElementById("modal06");
+  const modal07 = document.getElementById("modal07");
+  const modal08 = document.getElementById("modal08");
+  const modal09 = document.getElementById("modal09");
+  const modal10 = document.getElementById("modal10");
+  const modal11 = document.getElementById("modal11");
+  const modal12 = document.getElementById("modal12");
+  const modal13 = document.getElementById("modal13");
+  const modal14 = document.getElementById("modal14");
+  const modal15 = document.getElementById("modal15");
+  const modal16 = document.getElementById("modal16");
+  const modal17 = document.getElementById("modal17");
+
+
   const close = document.getElementById("close");
   const close02 = document.getElementById("close02");
+  const close03 = document.getElementById("close03");
+  const close04 = document.getElementById("close04");
+  const close05 = document.getElementById("close05");
+  const close06 = document.getElementById("close06");
+  const close07 = document.getElementById("close07");
+  const close08 = document.getElementById("close08");
+  const close09 = document.getElementById("close09");
+  const close10 = document.getElementById("close10");
+  const close11 = document.getElementById("close11");
+  const close12 = document.getElementById("close12");
+  const close13 = document.getElementById("close13");
+  const close14 = document.getElementById("close14");
+  const close15 = document.getElementById("close15");
+  const close16 = document.getElementById("close16");
+  const close17 = document.getElementById("close17");
 
 
 open.addEventListener("click", () => {
   modal.classList.remove("hidden");
   mask.classList.remove("hidden");
 });
-
 open02.addEventListener("click", () => {
   modal02.classList.remove("hidden");
   mask02.classList.remove("hidden");
 });
+open03.addEventListener("click", () => {
+  modal03.classList.remove("hidden");
+  mask03.classList.remove("hidden");
+});
+open04.addEventListener("click", () => {
+  modal04.classList.remove("hidden");
+  mask04.classList.remove("hidden");
+});
+open05.addEventListener("click", () => {
+  modal05.classList.remove("hidden");
+  mask05.classList.remove("hidden");
+});
+open06.addEventListener("click", () => {
+  modal06.classList.remove("hidden");
+  mask06.classList.remove("hidden");
+});
+open07.addEventListener("click", () => {
+  modal07.classList.remove("hidden");
+  mask07.classList.remove("hidden");
+});
+open08.addEventListener("click", () => {
+  modal08.classList.remove("hidden");
+  mask08.classList.remove("hidden");
+});
+open09.addEventListener("click", () => {
+  modal09.classList.remove("hidden");
+  mask09.classList.remove("hidden");
+});
+open10.addEventListener("click", () => {
+  modal10.classList.remove("hidden");
+  mask10.classList.remove("hidden");
+});
+open11.addEventListener("click", () => {
+  modal11.classList.remove("hidden");
+  mask11.classList.remove("hidden");
+});
+open12.addEventListener("click", () => {
+  modal12.classList.remove("hidden");
+  mask12.classList.remove("hidden");
+});
+open13.addEventListener("click", () => {
+  modal13.classList.remove("hidden");
+  mask13.classList.remove("hidden");
+});
+open14.addEventListener("click", () => {
+  modal14.classList.remove("hidden");
+  mask14.classList.remove("hidden");
+});
+open15.addEventListener("click", () => {
+  modal15.classList.remove("hidden");
+  mask15.classList.remove("hidden");
+});
+open16.addEventListener("click", () => {
+  modal16.classList.remove("hidden");
+  mask16.classList.remove("hidden");
+});
+open17.addEventListener("click", () => {
+  modal17.classList.remove("hidden");
+  mask17.classList.remove("hidden");
+});
+
 
 close.addEventListener("click", () => {
   modal.classList.add("hidden");
@@ -28,11 +154,117 @@ close02.addEventListener("click", () => {
   modal02.classList.add("hidden");
   mask02.classList.add("hidden");
 });
+close03.addEventListener("click", () => {
+  modal03.classList.add("hidden");
+  mask03.classList.add("hidden");
+});
+close04.addEventListener("click", () => {
+  modal04.classList.add("hidden");
+  mask04.classList.add("hidden");
+});
+close05.addEventListener("click", () => {
+  modal05.classList.add("hidden");
+  mask05.classList.add("hidden");
+});
+close06.addEventListener("click", () => {
+  modal06.classList.add("hidden");
+  mask06.classList.add("hidden");
+});
+close07.addEventListener("click", () => {
+  modal07.classList.add("hidden");
+  mask07.classList.add("hidden");
+});
+close08.addEventListener("click", () => {
+  modal08.classList.add("hidden");
+  mask08.classList.add("hidden");
+});
+close09.addEventListener("click", () => {
+  modal09.classList.add("hidden");
+  mask09.classList.add("hidden");
+});
+close10.addEventListener("click", () => {
+  modal10.classList.add("hidden");
+  mask10.classList.add("hidden");
+});
+close11.addEventListener("click", () => {
+  modal11.classList.add("hidden");
+  mask11.classList.add("hidden");
+});
+close12.addEventListener("click", () => {
+  modal12.classList.add("hidden");
+  mask12.classList.add("hidden");
+});
+close13.addEventListener("click", () => {
+  modal13.classList.add("hidden");
+  mask13.classList.add("hidden");
+});
+close14.addEventListener("click", () => {
+  modal14.classList.add("hidden");
+  mask14.classList.add("hidden");
+});
+close15.addEventListener("click", () => {
+  modal15.classList.add("hidden");
+  mask15.classList.add("hidden");
+});
+close16.addEventListener("click", () => {
+  modal16.classList.add("hidden");
+  mask16.classList.add("hidden");
+});
+close17.addEventListener("click", () => {
+  modal17.classList.add("hidden");
+  mask17.classList.add("hidden");
+});
+
 
 mask.addEventListener("click", () => {
   close.click();
 });
 mask02.addEventListener("click", () => {
   close02.click();
+});
+mask03.addEventListener("click", () => {
+  close03.click();
+});
+mask04.addEventListener("click", () => {
+  close04.click();
+});
+mask05.addEventListener("click", () => {
+  close05.click();
+});
+mask06.addEventListener("click", () => {
+  close06.click();
+});
+mask07.addEventListener("click", () => {
+  close07.click();
+});
+mask08.addEventListener("click", () => {
+  close08.click();
+});
+mask09.addEventListener("click", () => {
+  close09.click();
+});
+mask10.addEventListener("click", () => {
+  close10.click();
+});
+mask11.addEventListener("click", () => {
+  close11.click();
+});
+mask12.addEventListener("click", () => {
+  close12.click();
+});
+mask13.addEventListener("click", () => {
+  close13.click();
+});
+mask14.addEventListener("click", () => {
+  close14.click();
+});
+mask15.addEventListener("click", () => {
+  close15.click();
+});
+mask16.addEventListener("click", () => {
+  close16.click();
+});
+mask17.addEventListener("click", () => {
+  close17.click();
 });
 }
